ff25bb5739e07e7b291261415d72b899aa46efae
manuelrau <manuelrau@icloud.com>
2025-03-13 20:07:20 +0100

    alles auf anfang

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import {useEffect, useState} from 'react'
-import { getArticles } from "./callback/api.js"
+import { useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [articles, setArticles] = useState([])
-    useEffect(() => {
-        getArticles()
-            .then((data) => setArticles(data.data))
-            .catch((error) => console.log(error))
-    }, []);
+
   return (
       <>
           <div>
@@ -35,15 +29,6 @@ function App() {
               Click on the Vite and React logos to learn more
           </p>
           <h2>Funktioniert es?</h2>
-          <div>
-              <h1>Artikel aus Strapi</h1>
-              {articles.map((article) => (
-                  <div key={article.id}>
-                      <h2>{article.attributes.title}</h2>
-                      <p>{article.attributes.content}</p>
-                  </div>
-              ))}
-          </div>
       </>
   )
 }
